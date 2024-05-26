8600f4dbb640fd4f591d43fa3d33e5e8dd3fb623
Gorbenko Roman <45801227+rofle100lvl@users.noreply.github.com>
2024-05-26 10:51:50 +0200

    Added Xcodeproj file support

--- Sources/UseGraph/UseGraph.swift
+++ Sources/UseGraph/UseGraph.swift
@@ -4,109 +4,138 @@ import UseGraphCore
 
 enum PathError: Error {
     case pathIsNotCorrect
-}
-
-@main
-public struct UseGraphCommand: AsyncParsableCommand {
-    public init() { }
+    case shouldBeOnlyOnePath
     
-    public static let configuration = CommandConfiguration(
-        commandName: "usage_graph",
-        abstract: "Command to build graph of usage.",
-        version: "0.0.1"
-    )
-    
-    @Argument
-    var path: String
-    
-    @Argument
-    var format: String = "svg"
-    
-    @Option(help: ArgumentHelp(stringLiteral: "Путь к сорасам, на которых вы хотите дообучить модель"))
-    var educationalPath: String? = nil
-    
-    @Flag
-    var showLastChildren: Bool = false
-    
-    @Option
-    var excludedNames: String? = nil
-
-    public func run() async throws {
-        guard let url = URL(string: path) else {
-            throw PathError.pathIsNotCorrect
+    var localizedDescription: String {
+        switch self {
+        case .pathIsNotCorrect:
+            "Path is not correct. Check your path."
+        case .shouldBeOnlyOnePath:
+            "You should set strictly one path. Not a zero and not a both of them. Project or folder"
         }
+    }
+    
+    @main
+    public struct UseGraphCommand: AsyncParsableCommand {
+        public init() { }
         
-        let format = try OutputFormat.parse(format: format)
+        public static let configuration = CommandConfiguration(
+            commandName: "usage_graph",
+            abstract: "Command to build graph of usage.",
+            version: "0.0.1"
+        )
         
-        var scanResults = await InitScanner.scan(url: url)
-            .map(\.parametersDependencies)
-            .reduce([String: Set<String>]()) { result, element in
-                result.merging(element, uniquingKeysWith: {
-                    $0.union($1)
-                })
+        @Option(help: "Path to project (.xcodeproj)")
+        var projectPath: String? = nil
+        
+        @Option(help: "Path to folder with sources")
+        var folderPath: String? = nil
+        
+        @Argument(help: "Output file format. Now available: CSV, SVG, PNG, GV")
+        var format: String = "svg"
+        
+        @Option(help: ArgumentHelp(stringLiteral: "Path for the folder with sources you want to explore as well"))
+        var educationalPath: String? = nil
+        
+        @Flag(help: "If enabled all leasts will show you type of their variables")
+        var showLastChildren: Bool = false
+        
+        @Option(help: "Use if you want to exclude any entity names")
+        var excludedNames: String? = nil
+        
+        public func run() async throws {
+            if projectPath != nil && folderPath != nil {
+                throw PathError.shouldBeOnlyOnePath
             }
-        
-        if let educationalPath,
-           let url = URL(string: educationalPath) {
-            let fullScanResults = await InitScanner.scan(url: url)
-                .map(\.parametersDependencies)
-                .reduce([String: Set<String>]()) { result, element in
+            
+            if projectPath == nil && folderPath == nil {
+                throw PathError.shouldBeOnlyOnePath
+            }
+            
+            var url: URL?
+            
+            if let projectPath {
+                url = URL(string: projectPath)
+            } else if let folderPath {
+                url = URL(string: folderPath)
+            }
+            
+            guard let url else {
+                throw PathError.pathIsNotCorrect
+            }
+            
+            let format = try OutputFormat.parse(format: format)
+            
+            var scanResults = try await InitScanner.scan(url: url)
+                .map(\.fileScanResult)
+                .reduce([String: Node]()) { result, element in
                     result.merging(element, uniquingKeysWith: {
-                        $0.union($1)
+                        Node(moduleName: $0.moduleName, connectedTo: $0.connectedTo.union($1.connectedTo))
                     })
                 }
             
-            scanResults = unionDesigns(scanResults: scanResults, fullScanResults: fullScanResults)
-        }
-        
-        if let excludedNames {
-            excludedNames.split(separator: ", ").forEach {
-                scanResults.removeValue(forKey: String($0))
+            if let educationalPath,
+               let url = URL(string: educationalPath) {
+                let fullScanResults = try await InitScanner.scan(url: url)
+                    .map(\.fileScanResult)
+                    .reduce([String: Node]()) { result, element in
+                        result.merging(element, uniquingKeysWith: {
+                            Node(moduleName: $0.moduleName, connectedTo: $0.connectedTo.union($1.connectedTo))
+                        })
+                    }
+                
+                scanResults = unionDesigns(scanResults: scanResults, fullScanResults: fullScanResults)
             }
-        }
-        var results = scanResults
-        
-        if !showLastChildren {
-            results = scanResults
-                .reduce([String: Set<String>]()) { result, element in
+            
+            if let excludedNames {
+                excludedNames.split(separator: ", ").forEach {
+                    scanResults.removeValue(forKey: String($0))
+                }
+            }
+            var results = scanResults
+            
+            if !showLastChildren {
+                results = scanResults
+                    .reduce([String: Node]()) { result, element in
+                        var newResult = result
+                        let usedNodes = element.value.connectedTo.filter { scanResults.keys.contains($0) }
+                        newResult[element.key] = Node(moduleName: element.value.moduleName, connectedTo: usedNodes)
+                        return newResult
+                    }
+            }
+            
+            results = results
+                .reduce([String: Node]()) { result, element in
                     var newResult = result
-                    let usedNodes = element.value.filter { scanResults.keys.contains($0) }
-                    newResult[element.key] = usedNodes
+                    var set = element.value.connectedTo
+                    for to in set {
+                        if results.keys.contains(where: { $0 == element.key.appending(".").appending(to) }) {
+                            set.remove(to)
+                            set.insert(element.key.appending(".").appending(to))
+                        }
+                    }
+                    newResult[element.key] = Node(moduleName: element.value.moduleName, connectedTo: set)
                     return newResult
                 }
+            
+            GraphBuilder.shared.buildGraph(dependencyGraph: results, format: format)
         }
         
-        results = results
-            .reduce([String: Set<String>]()) { result, element in
-                var newResult = result
-                var set = element.value
-                for to in set {
-                    if results.keys.contains(where: { $0 == element.key.appending(".").appending(to) }) {
-                        set.remove(to)
-                        set.insert(element.key.appending(".").appending(to))
+        func unionDesigns(scanResults: [String: Node], fullScanResults: [String: Node]) -> [String: Node] {
+            var scanResultsCopy = scanResults
+            var notFound = true
+            repeat {
+                notFound = true
+                scanResultsCopy.forEach { element in
+                    element.value.connectedTo.forEach { to in
+                        if !scanResultsCopy.keys.contains(to) && fullScanResults.keys.contains(to) {
+                            scanResultsCopy[to] = fullScanResults[to]
+                            notFound = false
+                        }
                     }
                 }
-                newResult[element.key] = set
-                return newResult
-            }
-        
-        GraphBuilder.shared.buildGraph(dependencyGraph: results, format: format.toFormat())
-    }
-    
-    func unionDesigns(scanResults: [String: Set<String>], fullScanResults: [String: Set<String>]) -> [String: Set<String>] {
-        var scanResultsCopy = scanResults
-        var notFound = true
-        repeat {
-            notFound = true
-            scanResultsCopy.forEach { element in
-                element.value.forEach { to in
-                    if !scanResultsCopy.keys.contains(to) && fullScanResults.keys.contains(to) {
-                        scanResultsCopy[to] = fullScanResults[to]
-                        notFound = false
-                    }
-                }
-            }
-        } while(!notFound)
-        return scanResultsCopy
+            } while(!notFound)
+            return scanResultsCopy
+        }
     }
 }
--- Sources/UseGraphCore/Scanner.swift
+++ Sources/UseGraphCore/Scanner.swift
@@ -1,49 +1,91 @@
 import Foundation
 import SwiftSyntax
 import SwiftParser
+import XcodeProj
 
-extension String {
-  func removingPrefix(_ prefix: String) -> String {
-    guard self.hasPrefix(prefix) else { return self }
-    var urlString = String(self.dropFirst(prefix.count))
-    if urlString.hasPrefix("/") {
-      urlString = String(urlString.dropFirst())
-    }
-    return urlString
-  }
+public struct ModuleScanResult {
+    public let fileScanResult: [String: Node]
+    public let moduleName: String
 }
 
-public struct ScanResult {
+public struct FileScanResult {
     public let fileName: String
-    public let parametersDependencies: [String: Set<String>]
+    public let parametersDependencies: [String: Node]
 }
+
+public struct Node: Hashable {
+    public let moduleName: String
+    public let connectedTo: Set<String>
+    
+    public init(moduleName: String, connectedTo: Set<String>) {
+        self.moduleName = moduleName
+        self.connectedTo = connectedTo
+    }
+}
+
 
 public enum InitScanner {
-    public static func scan(url: URL) async -> [ScanResult] {
-        var filesToOptimize: [ScanResult] = []
+    public static func scan(url: URL) async throws -> [ModuleScanResult] {
+        var moduleScanResult: [ModuleScanResult] = []
         
-        let fileURLs = FileManager.default.allFiles(inDirectory: url)
+        var modules = [Module]()
+        
+        if url.pathExtension == "xcodeproj" {
+            modules = try XcodeprojManager.getAllModules(projUrl: url)
+        } else {
+            let fileURLs = FileManager.default.allFiles(inDirectory: url)
+            modules = [Module(moduleName: "", files: fileURLs)]
+        }
 
-        await withTaskGroup(of: ([String: Set<String>], String)?.self) { group in
-            for fileURL in fileURLs {
+        await withTaskGroup(of: ([FileScanResult], String)?.self) { group in
+            for module in modules {
                 group.addTask {
-                    return (await matchPattern(at: fileURL), fileURL.absoluteString.removingPrefix("file://"))
+                    return (await handleModule(module: module), module.moduleName)
                 }
             }
-            for await result in group {
-                if let result {
-                    if !result.0.isEmpty {
-                        filesToOptimize.append(
-                            ScanResult(
-                                fileName: result.1,
-                                parametersDependencies: result.0
-                            )
-                        )
+            for await entity in group {
+                if let entity {
+                    if !entity.0.isEmpty {
+                        let graph = entity.0
+                            .map(\.parametersDependencies)
+                            .reduce([String: Node]()) { result, element in
+                                
+                                result.merging(element, uniquingKeysWith: {
+                                    Node(moduleName: $0.moduleName, connectedTo: $0.connectedTo.union($1.connectedTo))
+                                })
+                            }
+
+                        moduleScanResult.append(ModuleScanResult(fileScanResult: graph, moduleName: entity.1))
                     }
                 }
             }
         }
-        return filesToOptimize
+        return moduleScanResult
+    }
+    
+    private static func handleModule(module: Module) async -> [FileScanResult] {
+        var fileGraphs = [FileScanResult]()
+        print(module.files.count)
+        await withTaskGroup(of: ([String: Set<String>], String).self) { group in
+            for file in module.files {
+                group.addTask {
+                    return (await matchPattern(at: file), file.path())
+                }
+                
+            }
+            for await entity in group {
+                let results = entity.0
+                    .reduce([String: Node]()) { result, element in
+                        var newResult = result
+                        newResult[element.key] = Node(moduleName: module.moduleName, connectedTo: element.value)
+                        
+                        return newResult
+                    }
+                fileGraphs.append(FileScanResult(fileName: entity.1, parametersDependencies: results))
+            }
+        }
+
+        return fileGraphs
     }
     
     private static func matchPattern(at fileURL: URL) async -> [String: Set<String>] {
@@ -61,7 +103,7 @@ public enum InitScanner {
     private static func findDependencies(dir: URL) throws -> [String: Set<String>] {
         let text = try String(contentsOf: dir, encoding: .utf8)
         let rootNode: SourceFileSyntax = Parser.parse(source: text)
-        let visitor = DefinitionVisitor(converter: SourceLocationConverter(fileName: dir.absoluteString, tree: rootNode))
+        let visitor = DefinitionVisitor()
         visitor.walk(rootNode)
         return visitor.graphDependencies
     }
