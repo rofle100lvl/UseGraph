1843a921ae0b876d2911bbe5db1888b6682991b7
Gorbenko Roman <45801227+rofle100lvl@users.noreply.github.com>
2024-06-08 15:17:42 +0200

    Added frontemd Layer

---
+++ Sources/UseGraphFrontend/UseGraph.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Roman Gorbenko on 08.06.2024.
+//
+
+import Foundation
+import ArgumentParser
+
+public struct UseGraphFrontendCommand: AsyncParsableCommand {
+    public init() { }
+    
+    public static let configuration = CommandConfiguration(
+      commandName: "use_graph",
+      abstract: "Swift CLI to work with Use Graph tool",
+      version: "0.0.1",
+      subcommands: [
+        UseGraphBuildCommand.self,
+        UseGraphAnalyzeCommand.self
+      ]
+    )
+}
